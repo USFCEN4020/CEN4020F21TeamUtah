daa26df716a20c386adde4239664d6514df53f7a
Jose Ramirez <proyectoramirez07@gmail.com>
2021-10-31 20:59:54 -0400

    Add user_utils functions used in messages

--- src/user_utils.py
+++ src/user_utils.py
@@ -11,3 +11,35 @@ def get_user():
     tuple = c.fetchone()
     # print(type(tuple))
     return tuple
+
+
+def is_plus(user):
+    query = """SELECT isPlus FROM Username WHERE username = ?"""
+    data = (user,)
+    c.execute(query, data)
+    tuple = c.fetchone()
+    return bool(tuple[0])
+
+
+def are_friends(user1, user2):
+    query = """
+        SELECT userOne, userRequested FROM Friends
+        WHERE ((userOne = ? AND userRequested = ?) 
+            OR (userOne = ? AND userRequested = ?))
+            AND request = 2
+    """
+
+    data = (user1, user2, user2, user1)
+    c.execute(query, data)
+    tuple = c.fetchone()
+
+    return tuple != None
+
+
+def is_user(user):
+    query = """SELECT username FROM Username WHERE username = ?"""
+    data = (user,)
+    c.execute(query, data)
+    tuple = c.fetchone()
+
+    return tuple != None
