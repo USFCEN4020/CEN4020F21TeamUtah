95dc0adc71d40586f67da447265ae7bb45091107
Jose Ramirez <proyectoramirez07@gmail.com>
2021-10-31 20:58:58 -0400

    Extend menu class to show text between title and options

--- src/menu.py
+++ src/menu.py
@@ -5,6 +5,7 @@ from typing import Callable, Dict
 
 class Menu:
     """base class for all menus"""
+
     def __init__(self) -> None:
         self.title: str = str()
         self.subtitle: str = str()
@@ -13,13 +14,17 @@ class Menu:
         self.generic_err: str = str()
         self.prompt: str = "Select an option from the menu: "
         self.return_option_text: str = "Go back\n"
-        self.invalid_input_err: str = f"{Fore.RED}Invalid Selection{Style.RESET_ALL}\n"  # makes text red
+        # makes text red
+        self.invalid_input_err: str = f"{Fore.RED}Invalid Selection{Style.RESET_ALL}\n"
         self.options: Dict[str, Callable] = IOdict()
         self.options[self.return_option_text] = self._end
 
     def __repr__(self) -> str:
-        self.options.move_to_end(self.return_option_text, False) # return option must always be first
+        # return option must always be first
+        self.options.move_to_end(self.return_option_text, False)
         output: str = f"\n{self.title}\n{self.subtitle}\n"  # menu header
+        output += self.pre_options()
+
         for i, key in enumerate(self.options.keys()):  # menu options
             output += f"{i} - {key}\n"
         return output
@@ -42,6 +47,9 @@ class Menu:
         """exit the menu"""
         print(self.exit_text)
         return True
+
+    def pre_options(self):
+        return ""
 
     def run(self) -> None:
         """run the menu"""
